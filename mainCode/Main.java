6924ea8e87bfd0a5021227b9f0f99d0880fa3288
jane-csj <121679830+jane-csj@users.noreply.github.com>
2023-08-12 01:29:46 +0800

    Add files via upload

---
+++ mainCode/Main.java
@@ -0,0 +1,29 @@
+
+import java.io.*;
+
+
+    // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+    // then press Enter. You can now see whitespace characters in your code.
+    public  class Main extends Achieve {
+
+        public static void main(String[] args) throws IOException {
+            Student student = new Student("name", "男", 18, 19, "computer", 30, 20);
+            student.setFinished(false);
+            student.add_message();
+
+            Main m = new Main();
+
+
+            System.out.println(m.calculate("学生", 21));
+        }
+    }
+
+
+
+
+
+
+
+
+
+
